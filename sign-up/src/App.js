ca330ae50dc26bbd5f71618ab16a9f256d983697
MartinNey <nielumartin@hotmail.com>
2017-03-13 15:16:03 +0800

    idea vcs??

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SignForm from './sign-form';
+import SignForm from './components/sign-form.js';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app-main">
+        <nav>
           <img src={logo} className="app-logo" alt="logo" />
-          <h2>You Pai</h2>
-          <SignForm>
-          </SignForm>
-        </div>
+          <h2 className="app-intro">友拍 内推摄影师审核</h2>
+        </nav>
+        <SignForm />
       </div>
     );
   }
